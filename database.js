f4930dc443b97c61766beb4f652364dc69f64198
Miguelms017 <miguelmartinez030205@gmail.com>
2025-10-23 15:35:38 -0400

    change DB info

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2'
 
 const pool = mysql.createPool({
     host: 'sql5.freesqldatabase.com',
-    user: 'sql5799947',
-    password: 'qhgWePf2IG',
-    database: 'sql5799947'
+    user: 'sql5804260',
+    password: 'ajTzunT6DS',
+    database: 'sql5804260'
 }).promise()
 
 export async function create(Fname, Lname, phone, email, date, hairstylist, service, description) {
